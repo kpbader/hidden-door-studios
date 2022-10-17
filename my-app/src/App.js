077ad4b945f9efbc2b339bc694e55cc097a93211
Kyle Patrick Bader <kbader91@gmail.com>
2022-10-17 12:05:48 -0700

    App.js file set up with initial structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from "./components/Home/home";
+import Services from "./components/Services/services";
+import Gallery from "./components/Gallery/gallery";
+import About from "./components/About/about";
+import Contact from "./components/Contact/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<Home />}></Route>
+          <Route path="/Services" element={<Services />}></Route>
+          <Route path="/Gallery" element={<Gallery />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
