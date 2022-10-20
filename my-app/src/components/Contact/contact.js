f84939f1fa9fa8eb9557e49fbef260d911632ca0
Kyle Patrick Bader <kbader91@gmail.com>
2022-10-20 13:52:39 -0700

    contact section implemented

--- my-app/src/components/Contact/contact.js
+++ my-app/src/components/Contact/contact.js
@@ -1,21 +1,65 @@
 import './contact.css';
 import jT from '../../assets/images/joshua-tree-1.jpg';
+import { useState, useRef } from 'react';
+// import mailOutline from "../../assets/icons/mail-outline.svg";
+// import locationPin from "../../assets/icons/location-outline.svg";
+// import phoneIcon from "../../assets/icons/call-outline.svg";
+import { BsEnvelope, BsTelephone } from 'react-icons/bs';
+import { SlLocationPin } from "react-icons/sl";
 
 function Contact() {
 
+    const form = useRef();
+    const [contactForm, setContactForm] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = contactForm;
+    const [errorMessage, setErrorMessage] = useState('');
+
     return (
-        <section id="contact-section">
-            <div id="contact-splash">
-                <div className="splash-text">Contact</div>
-                <img src={jT} alt="joshua tree" id="sliderIMG" />
-            </div>
-            <div id="contact">
-                {/* <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12182.30520634488!2d-74.0652613!3d40.2407219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1561060983193!5m2!1sen!2sus"
-                    frameborder="0" style="border:0" allowfullscreen>
-                </iframe> */}
-            </div>
-        </section>
+        <>
+            <section id="contact-section">
+                <div id="contact-splash">
+                    <div className="splash-text">Contact</div>
+                    <img src={jT} alt="joshua tree" id="sliderIMG" />
+                </div>
+                <div id="contact-container">
+                    <form id="contact-form">
+                        <h2>Reach Out</h2>
+                        <div id="form-slot-1">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" defaultValue={name} name="name" />
+                        </div>
+                        <div id="form-slot-2">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" defaultValue={email} name="email" />
+                        </div>
+                        <div id="form-slot-3">
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" defaultValue={message} rows="10" cols="45"/>
+                        </div>
+                        <button type="submit" className="contact-submit-btn">Submit</button>
+                    </form>
+                    <div id="info-and-map">
+                        <div id="contact-info">
+                            <div className="contact-info-line">
+                                <BsEnvelope />
+                                <p>info@hiddendoorstudios.com</p>
+                            </div>
+                            <div className="contact-info-line">
+                                <BsTelephone />
+                                <p>(310) 888-8888</p>
+                            </div>
+                            <div className="contact-info-line">
+                                <SlLocationPin />
+                                <p>1234 Melrose Ave. West Hollywood, CA 90046</p>
+                            </div>
+                        </div>
+                        <div id="google-map">
+                            <iframe title="map" width="500" height="400" src="https://maps.google.com/maps?q=1800%20Vine%20Street%20Los%20Angle&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 };
 
