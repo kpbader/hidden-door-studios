77e56b66324d72f54d8dfdf409bc214302ac0011
Kyle Patrick Bader <kbader91@gmail.com>
2022-10-20 14:01:32 -0700

    google map/address updated

--- my-app/src/components/Contact/contact.js
+++ my-app/src/components/Contact/contact.js
@@ -50,11 +50,11 @@ function Contact() {
                             </div>
                             <div className="contact-info-line">
                                 <SlLocationPin />
-                                <p>1234 Melrose Ave. West Hollywood, CA 90046</p>
+                                <p>7561 Melrose Ave. West Hollywood, CA 90046</p>
                             </div>
                         </div>
                         <div id="google-map">
-                            <iframe title="map" width="500" height="400" src="https://maps.google.com/maps?q=1800%20Vine%20Street%20Los%20Angle&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.437250098534!2d-118.35691804914127!3d34.083937480502065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bed3abba7fdb%3A0x3b9fba3f4044dc43!2s7561%20Melrose%20Ave%2C%20Los%20Angeles%2C%20CA%2090046!5e0!3m2!1sen!2sus!4v1666299489629!5m2!1sen!2sus" title="map" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                 </div>
